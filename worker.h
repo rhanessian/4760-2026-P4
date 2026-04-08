782f453d9ecd5145a97d8d41ce413a7eb92f8a73
Cloud Hanessian <cloudhanessian@MacBookPro.lan>
2026-04-07 23:34:21 -0500

    files updated

--- worker.h
+++ worker.h
@@ -17,27 +17,25 @@
 
 
 
-#define MAXPROC 20
+#define MAXPROC 18
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define MAXTEXT 200
 #define PERMS 0644
 
 struct PCB{
-	bool occupied;	//is the slot being used
-	int launched;	//number launched (if 5th then 5)   
-    pid_t pid;  	//pid of child
-    bool active;	//is child still running     
-    int startS;		//time created seconds
-    int startN;		//time created nanoseconds
-    int serviceTimeSec; //total seconds it has been scheduled
-    int serviceTimeNano; //total nanoseconds it has been scheduled
-    int eventWaitSec;	//when does its event happen
-    int eventWaitNano;	//when does its event happen
-    int blocked;	//is this process waiting on an event
-//	int termS;		
-//  int termN;
-//  int msgsSent;
+	bool occupied;			// is the slot being used
+	int launched;			// number launched (if 5th then 5)   
+    pid_t pid;  			// pid of child
+    bool active;			// is child still running     
+    int startS;				// time created seconds
+    int startN;				// time created nanoseconds
+    int serviceTimeSec; 	// total seconds it has been scheduled
+    int serviceTimeNano; 	// total nanoseconds it has been scheduled
+    int eventWaitSec;		// when does its event happen
+    int eventWaitNano;		// when does its event happen
+    bool blocked;			// is this process waiting on an event
+    int msgsSent; 			// total number of messages received from OSS
 };
 
 struct sharedMem{
